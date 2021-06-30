7d9814742cff72ffe7b1aa28c05f471b9ee0c8f1
James Ashcroft <james.ashcroft@engineering.digital.dwp.gov.uk>
2021-06-30 18:07:54 +0100

    Added new query function to prep food

--- app/routes.js
+++ app/routes.js
@@ -7,23 +7,25 @@ const router = express.Router()
 
 // current sprint 26 // *****************************************************************************************************************
 //Create query preparing food activity
-router.post('/current/queries/create-query', (req, res, next) => {
-  console.log('/current/queries/create-query', req.session.data)
+// router.post('/current/activities/preparing-food', (req, res, next) => {
+router.post('/current/activities/preparing-food', (req, res, next) => {
+  console.log('/current/activities/preparing-food', req.session.data)
   const name = req.session.data['query-content']
   const section = req.session.data.source
-
   const queries = req.session.data.queries || []
   queries.push({ name, section })
   req.session.data.queries = queries
   res.redirect('/current/set-action/set-action-preparing-food')
 })
 
+
 router.post('/current/set-action/set-action-preparing-food', (req, res, next) => {
   console.log('this is preparing food')
   console.log(req.session.data)
+  req.session.data.queries[req.session.data.queries.length - 1].contentQ = req.session.data['query-content']
   req.session.data.queries[req.session.data.queries.length - 1].action = req.session.data['set-an-action']
   console.log(1, req.session.data)
-  res.redirect('/current/task-list')
+  res.redirect('/current/activities/preparing-food')
 })
 
 //Create query taking nutrition activity
